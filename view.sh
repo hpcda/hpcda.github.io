930563ae37d186b54e567cf62c8a45042d10072a
Julien Bigot <julien.bigot@cea.fr>
2021-04-16 15:51:58 +0200

    update bruno-raffin

--- view.sh
+++ view.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 cd "$(dirname "$0")"
-rm -r resources
-hugo server --disableFastRender --i18n-warnings -p 1316
-rm -r resources
+[ -d resources ] && rm -r resources
+docker run -p 1316:1316 "-u$(id -u):$(id -g)" --rm -it -v "$(pwd):/src" klakegg/hugo:0.81.0-ext-alpine server --disableFastRender --i18n-warnings -p 1316
+[ -d resources ] && rm -r resources
